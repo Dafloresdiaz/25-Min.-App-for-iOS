6e77f0ae9ce2fc422e0cb1424aef48499181e666
dafloresdiaz <dafloresdiaz@gmail.com>
2020-06-25 15:50:03 -0500

    Created the connections
    wiht the connection created, I added a new label to show the current date. I think it would give a good desing knowing the date

--- Pomodoro/ViewController.swift
+++ Pomodoro/ViewController.swift
@@ -9,9 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let formatter = DateFormatter()
+    let date = Date()
+    
+    
+    @IBOutlet weak var getCurrentDate: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func startTime(_ sender: Any) {
+        formatter.dateFormat = "dd-MM-yyyy"
+        getCurrentDate.text = formatter.string(from: date)
     }
     
 }
