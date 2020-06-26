d8d265510863b7207a6536bc9f5cf5929b7fa8f3
dafloresdiaz <dafloresdiaz@gmail.com>
2020-06-25 22:26:26 -0500

    Created the class and begin the timer functions
    Finished the class to ibtain the date and start doing the functionality for the timer and the play button to start the timer

--- Pomodoro/ViewController.swift
+++ Pomodoro/ViewController.swift
@@ -8,21 +8,46 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
     
-    let formatter = DateFormatter()
-    let date = Date()
-    
-    
     @IBOutlet weak var getCurrentDate: UILabel!
+    @IBOutlet weak var getTimerUpdate: UILabel!
+    //25 min are 1500 seconds
+    var secondsTotal = 60
+    
+    let getDateString = getDate()
+    var timer = Timer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        getCurrentDate.text = getDateString.getDateStringFormat()
+        
     }
     
     @IBAction func startTime(_ sender: Any) {
-        formatter.dateFormat = "dd-MM-yyyy"
-        getCurrentDate.text = formatter.string(from: date)
+        timer = Timer.scheduledTimer(timeInterval: 1.0,
+                                     target: self,
+                                     selector: #selector(getTimerRunning),
+                                     userInfo: nil, repeats: true)
+    }
+    
+    @objc func getTimerRunning(){
+        if (secondsTotal > 0){
+            let minutes = Int(secondsTotal / 60 % 60)
+            let seconds = Int(secondsTotal % 60)
+            
+            if (minutes < 10){
+                getTimerUpdate.text = "0\(minutes):\(seconds)"
+            }
+            if (minutes < 10 && seconds < 10) {
+                getTimerUpdate.text = "0\(minutes):0\(seconds)"
+            }
+            secondsTotal -= 1
+        } else {
+            secondsTotal = 300
+            getTimerUpdate.text = "05:00"
+        }
     }
     
 }
---
+++ Pomodoro/getLabels/getDate.swift
@@ -0,0 +1,23 @@
+//
+//  getDate.swift
+//  Pomodoro
+//
+//  Created by Daniel Flores on 25/06/20.
+//  Copyright © 2020 Daniel Flores. All rights reserved.
+//
+
+import Foundation
+
+class getDate {
+    
+    let formatter = DateFormatter()
+    let date = Date()
+    
+    func getDateStringFormat() -> String {
+        // This return the date with a type string
+        formatter.dateFormat = "dd/MM/yyyy"
+        let dateText = formatter.string(from: date)
+        return dateText
+    }
+    
+}
